5ade42f98660a649c1db5dbaa254f0058dde4814
Eric-study-369 <jakeson270@gmail.com>
2025-01-14 14:51:56 +0700

    feat:lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/ecommerce_app/models/shop.dart';
-import 'package:flutter_tutorial/ecommerce_app/page/card_page.dart';
-import 'package:flutter_tutorial/ecommerce_app/page/intro_page.dart';
-import 'package:flutter_tutorial/ecommerce_app/page/shop_page.dart';
-import 'package:flutter_tutorial/ecommerce_app/thmes/light_mode.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => Shop(),
-      child: const MyApp(),
-    ),
+    const MyApp(),
   );
 }
 
@@ -20,15 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Intropage(),
-      theme: lightMode,
-      routes: {
-        '/intro_page': (context) => const Intropage(),
-        '/shop_page': (context) => const ShopPage(),
-        '/card_page': (context) => const CardPage(),
-      },
-    );
+    return MaterialApp();
   }
 }
