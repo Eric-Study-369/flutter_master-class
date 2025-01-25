f9dde4e749473ee6cf0fe6deeb4ecfae9cde74cb
Eric-study-369 <jakeson270@gmail.com>
2025-01-25 14:52:38 +0700

    Initialize(new project): habit tacker

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/models/note_database.dart';
-import 'package:flutter_tutorial/page/note_page.dart';
-import 'package:flutter_tutorial/them/theme_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_tutorial/page/home_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await NoteDatabase.initialize();
-  runApp(
-    MultiProvider(
-      providers: [
-        // Note provider
-        ChangeNotifierProvider(create: (context) => NoteDatabase()),
-
-        //theme provider
-        ChangeNotifierProvider(create: (context) => ThemProvider())
-      ],
-      child: const MyApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Notespage(),
-      theme: Provider.of<ThemProvider>(context).themeData,
+      home: HomePage(),
     );
   }
 }
